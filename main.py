0059c0f84bc834f1537c8e78826db96eab9d7726
Vishal Sahare <vsahare@cisco.com>
2026-09-25 11:38:26 +0530

    create llm and stdio server params objects

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import asyncio
-import os
 
 from dotenv import load_dotenv
+
 load_dotenv()
+
+from langchain.agents import create_agent
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/vsahare/Desktop/Vishal/Personal/Vishal/Padhai/Udemy/EdemMCPServerCourse/mcp-servers/langchain-mcp-adapters-usage/servers/math_server.py"],
+)
 
 async def main():
     print("Hello from langchain-mcp-adapters-usage!")
